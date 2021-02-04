2d14fd28b9440d1238c3b7d298a9493678f1ec0b
Maika <maika@Amys-MBP.lan>
2021-02-03 22:13:52 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,10 @@
 module ArtistsHelper
+    def display_artist(song)
+        if song.artist
+            artist = song.artist
+            link_to artist.name, artist_path(artist)
+        else song.artist == []
+            link_to "Add Artist", edit_song_path(song)
+        end
+    end
 end
